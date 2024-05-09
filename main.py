33ffe81aaf6e1269b620f90643c4cd4624767aaa
김우정 <dnwjd4336@naver.com>
2024-05-09 18:32:33 +0900

    ++

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ st.title("과의존 위험군?")
 # 이름을 입력 받고 session_state에 저장
 name = st.text_input('이름을 입력하세요!')
 st.session_state.name = name
-sex = st.text_input('성별을 입력하세요!')
+sex = st.text_input('성별을 입력하세요! (남, 여)')
 st.session_state.sex = sex
 age = st.text_input('나이를 입력하세요!')
 st.session_state.age = age
