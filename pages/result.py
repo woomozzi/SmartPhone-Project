a7db6098f5336f201473a65a2886ce9f25d0dc9c
김우정 <dnwjd4336@naver.com>
2024-05-13 09:46:59 +0900

    시각화 제거

--- pages/result.py
+++ pages/result.py
@@ -5,8 +5,6 @@ import matplotlib.pyplot as plt
 plt.rcParams['font.family'] ='Malgun Gothic'
 plt.rcParams['axes.unicode_minus'] =False
 
-# 시각화에 사용할 데이터 로드
-df = pd.read_csv("C:\WEBDEVELOP_0507\data\webdata.csv", encoding='cp949')
 
 # ------------ 점수 변수 초기화 코드-------------
 if 'total_score' not in st.session_state:
@@ -64,50 +62,3 @@ else :
         overdose_adult(total_score_adult)
     elif age >= 60:
         overdose_adult_senior(total_score_adult)
-
-
-# --------------나이에 따른 위험군 분포 코드--------------------------
-
-df_2 = df[(df['가구원_연령'] >= (age-3)) & (df['가구원_연령'] <= (age+3))]
-df_2['성별'] = df_2['성별'].replace({1:'남', 2:'여'})
-df_2 = df_2[df_2['성별'] == sex]
-df_3 = pd.DataFrame(df_2['위험군'].value_counts())
-df_4 = pd.DataFrame(df_2['설문합계'].value_counts())
-df_5 = pd.DataFrame(df_2['14.여가시간 주요 활동-1순위'].value_counts().reset_index())
-df_5 = df_5.loc[df_5['14.여가시간 주요 활동-1순위'] != 0.0]
-df_5['14.여가시간 주요 활동-1순위'] = df_5['14.여가시간 주요 활동-1순위'].replace({1.0 : "문화예술 관람 및 참여",
-                                                               2.0 : "스포츠 관람 및 참여",
-                                                               3.0 : "취미, 오락 활동(쇼핑/외식 등)",
-                                                               4.0 : "스마트폰 이용",
-                                                               5.0 : "관광활동(자연명승 관람, 캠핑 등)",
-                                                               6.0 : "휴식활동(TV시청, 산책 및 걷기 등)",
-                                                               7.0 : "사회 및 기타활동(친구만남, 종교활동 등)",
-                                                               8.0 : "기타"})
-
-plt.rcParams.update({'font.size': 16})
-
-fig, ax = plt.subplots(figsize=(18, 8))
-ax.bar(df_4.index, df_4['count'])
-# 그래프에 제목 추가
-plt.title('당신의 나이와 비슷하고 성별이 {}인 사람들의 점수표'.format(sex))
-# Matplotlib figure 객체를 Streamlit에 전달
-st.pyplot(fig)
-
-fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(18, 8))
-
-# 첫 번째 그래프: 파이 차트 (위험군 비율)
-sizes = df_3['count']
-labels = df_3.index
-ax1.pie(sizes, labels=labels, autopct='%1.1f%%')
-ax1.set_title('당신의 나이와 비슷하고 성별이 {}자인 사람들의 위험군 비율'.format(sex))
-
-# 두 번째 그래프: 파이 차트 (여가시간 주요활동)
-sizes = df_5['count']
-labels = df_5['14.여가시간 주요 활동-1순위']
-ax2.pie(sizes, labels=labels, autopct='%1.1f%%')
-ax2.set_title('당신의 나이와 비슷하고 성별이 {}자인 사람들의 여가시간 주요활동'.format(sex))
-
-
-plt.tight_layout()  # 각 그래프 간의 간격 조정
-st.pyplot(fig)
-# ----------------------------------------------------------------
