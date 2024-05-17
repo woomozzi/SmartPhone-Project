f2d1655580fab3a42607c79497dd325a0ed5ae5d
김우정 <dnwjd4336@naver.com>
2024-05-17 17:46:06 +0900

    ui수정

--- pages/main.py
+++ pages/main.py
@@ -5,15 +5,18 @@ st.set_page_config(
     page_icon = "👋"
 )
 
-st.title("당신의 스마트폰 과의존 위험군을 진단 해보세요")
+st.title("당신의 스마트폰 과의존 위험군을")
+st.title("진단 해보세요📱")
+st.markdown("---")
 
 # 이름을 입력 받고 session_state에 저장
 name = st.text_input('이름을 입력하세요! (자녀 진단시 자녀이름)')
 st.session_state.name = name
-sex = st.text_input('성별을 입력하세요! (남,여)')
+sex = st.text_input('성별을 입력하세요! (남 / 여)')
 st.session_state.sex = sex
 age = st.text_input('나이를 입력하세요! (자녀 진단시 자녀나이)')
 st.session_state.age = age
+st.markdown("---")
 
 st.subheader('👧 kids : 3-9세 자녀를 둔 부모')
 st.page_link("pages/kids.py", label="kids", icon="✔️")
--- realmain.py
+++ realmain.py
@@ -1,10 +1,13 @@
 import streamlit as st
 
-st.title("스마트폰 과의존 위험군")
+st.title("S.T.O.P")
+st.write("smartphone toxic overcome project")
 st.markdown("---")
 embed_code = """
 <iframe title="project2_도입부" width="800" height="400" src="https://app.powerbi.com/view?r=eyJrIjoiMjFlNjI0NjUtNzAxNi00N2EwLWIxNTMtMWEyNjEzYWZmM2YzIiwidCI6ImU0ZmVkY2NkLWRlOWMtNGUxMS04NDY3LWI0Y2FjMTliYzIzMyJ9" frameborder="0" allowFullScreen="true"></iframe>
 """
 st.markdown(embed_code, unsafe_allow_html=True)
 
-st.page_link("pages/main.py", label="설문하러가기~", icon="✔️")
+st.markdown("---")
+
+st.page_link("pages/main.py", label="설문하러가기", icon="✔️")
